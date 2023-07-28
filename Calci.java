44b5b3f9f13b1375f6af61612a923d2d1af78c94
Thejaswi25 <tpoojarythejaswi@gmail.com>
2023-07-28 19:06:25 +0530

    Provided imp for Calci interface

--- Calci.java
+++ Calci.java
@@ -9,3 +9,15 @@ int getSum(int n1, int n2);
  return n1+n2;
  }
  }
+ public class CalciDriver 
+public  static void main (String[]args)
+{
+Scanner s=new Scanner(System.in);
+system.out.println("Enter 2 numbers to find Sum");
+int n1=s.nextInt();
+int n2=s.nextInt();
+CalciImpl c=new CalciImp();
+int Sum=c.getSum(n1,n2);
+system.out.println(n1 +"+n2+"sum");
+}
+}
